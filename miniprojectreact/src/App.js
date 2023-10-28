f1dbdb558e52682534485f39c8604531552c4091
lianne <liannealq@hotmail.com>
2023-10-28 20:38:06 +0300

    ..

--- miniprojectreact/src/App.js
+++ miniprojectreact/src/App.js
@@ -1,8 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App">HELLO</div>;
+  return (
+    <div className="App">
+      HELLO
+      <Routes>
+        <Route path="/" Component={Home}>
+          {" "}
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
