c2b9b9b74da6067434a632eac99e1eca9f533452
lianne <liannealq@hotmail.com>
2023-10-29 00:36:50 +0300

    imglink

--- models/Book.js
+++ models/Book.js
@@ -2,9 +2,14 @@ const { model, Schema } = require("mongoose");
 
 const booksSchema = new Schema({
   title: { type: String, required: true },
-  author: String,
+  author: { type: String, required: true },
   price: { type: Number, required: false, default: 5 },
-  image: { type: String, required: false, default: "link" },
+  image: {
+    type: String,
+    required: false,
+    default:
+      "https://i.pinimg.com/originals/18/cb/30/18cb30dc852a352f8fb7b28fc8f18319.jpg",
+  },
 });
 
 module.exports = model("books", booksSchema); // books , booksScheme
