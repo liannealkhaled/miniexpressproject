b144aa8a012293ac5182a8711057a05959431eb6
lianne <liannealq@hotmail.com>
2023-10-28 21:41:59 +0300

    .

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const cors = require("cors");
 const connectDB = require("./database");
 const booksrouter = require("./api/Books/routes");
 
 app.use(express.json());
 
 app.use("/api", booksrouter);
+app.use(cors());
 
 connectDB();
 app.listen(process.env.PORT, () => {
