50e2557108e208216c7613900a6e07f5a509993c
lianne <liannealq@hotmail.com>
2023-10-29 16:24:07 +0300

    imageuploadinappjs

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const cors = require("cors");
 const connectDB = require("./database");
 const booksrouter = require("./api/Books/routes");
 
+app.use(express.json());
+app.use(cors());
+app.use("/api", booksrouter);
+
+//////// image upload ::
 const multer = require("multer");
 //Setting storage engine
 const storageEngine = multer.diskStorage({
@@ -17,12 +22,17 @@ const storageEngine = multer.diskStorage({
 const upload = multer({
   storage: storageEngine,
 });
-app.use(express.json());
-app.use(cors());
-app.use("/api", booksrouter);
+app.post("/single", upload.single("image"), (req, res) => {
+  if (req.file) {
+    res.send("Single file uploaded successfully");
+  } else {
+    res.status(400).send("Please upload a valid image");
+  }
+});
+
+/// end of image upload
 
 connectDB();
-
 app.listen(process.env.PORT, () => {
   console.log("The application is running on localhost:8000");
 });
