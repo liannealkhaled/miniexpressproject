71d0efd68665550aa00cdfd3afdbdfcc2e9651ff
lianne <liannealq@hotmail.com>
2023-10-29 16:12:01 +0300

    anotherImageFolder

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const cors = require("cors");
 const connectDB = require("./database");
 const booksrouter = require("./api/Books/routes");
 
+const multer = require("multer");
+//Setting storage engine
+const storageEngine = multer.diskStorage({
+  destination: "./images",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}--${file.originalname}`);
+  },
+});
+//initializing multer
+const upload = multer({
+  storage: storageEngine,
+});
 app.use(express.json());
 app.use(cors());
 app.use("/api", booksrouter);
